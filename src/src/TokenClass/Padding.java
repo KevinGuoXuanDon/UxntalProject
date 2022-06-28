7c5f63858be7506c8873c77a6663fe4903d224e9
Xuandong Guo <2427543166@qq.com>
2022-06-28 23:02:26 +0100

    update Token class

--- src/src/TokenClass/Padding.java
+++ src/src/TokenClass/Padding.java
@@ -1,2 +1,38 @@
-package TokenClass;public class Padding {
+package TokenClass;
+
+public class Padding {
+    char indication;
+    String content;
+    public Padding(char indication, String content){
+        this.indication = indication;
+        this.content = content;
+    }
+
+    public boolean isPadding(char indication){
+        return isAbsolute(indication) || isRelative(indication);
+    }
+
+    public boolean isAbsolute(char indication){
+        return indication =='|';
+    }
+
+    public boolean isRelative(char indication){
+        return indication == '$';
+    }
+
+    public char getIndication() {
+        return indication;
+    }
+
+    public void setIndication(char indication) {
+        this.indication = indication;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
