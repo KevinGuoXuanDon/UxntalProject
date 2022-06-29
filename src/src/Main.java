36b49c017848027cf88e8c694454b90fb4cb3c25
Xuandong Guo <2427543166@qq.com>
2022-06-29 22:03:25 +0100

    create a main method to call all functions or models

---
+++ src/src/Main.java
@@ -0,0 +1,71 @@
+import Model.TokenClass.AddressLabel;
+import utils.ReadAndClean;
+import Model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    static ReadAndClean read = new ReadAndClean();
+    static FunctionDetermine functionModel = new FunctionDetermine();
+    static List<FunctionDetermine> functionList = new ArrayList<>();
+    static List<AddressLabel> AddressLabelList = new ArrayList<>();
+
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        Map<String,List<String>> tal = read.splitBlock(read.readUnxtalByWord());
+        System.out.println("Here is the result :");
+        if(tal!=null){
+            for(String s:tal.keySet()){
+                System.out.println(s+"   "+tal.get(s));
+            }
+        }
+        // deal with rest-lables
+        final char start = '@';
+        int mark = 0;
+        List<String> lables = tal.get("rest-lables");
+        List<String> recorder = new ArrayList<>();
+        for(int i =0; i<lables.size(); i++){
+            String str =  lables.get(i);
+            if(str.charAt(0)==start){
+                mark++;
+            }
+            if(mark==1){
+                recorder.add(str);
+                if(functionModel.isFunction(start,str)){
+                    mark--;
+                    String name = recorder.get(0).substring(1);
+                    String end = str;
+                    FunctionDetermine newfunction = new FunctionDetermine(name,end,recorder);
+                    recorder.clear();
+                    functionList.add(newfunction);
+                    System.out.println("function added "+name);
+                }
+                else if(i==lables.size()-1){
+                    AddressLabel newAddress = new AddressLabel(start,recorder);
+                    AddressLabelList.add(newAddress);
+                    recorder.clear();
+                    mark--;
+                }
+            }
+            else if(mark==2){
+                AddressLabel newAddress = new AddressLabel(start,recorder);
+                AddressLabelList.add(newAddress);
+                recorder.clear();
+                mark--;
+                recorder.add(str);
+            }
+        }
+        System.out.println();
+        System.out.println("Here is your functions");
+        for(FunctionDetermine f :functionList){
+            System.out.println("function's name: "+f.getName());
+        }
+        System.out.println("Here is your AddressLable");
+        for(AddressLabel al: AddressLabelList){
+            System.out.println("AddressLabel :"+al.getContent());
+        }
+    }
+}
